fc08d647c7a3c641464048c3f4fd75eb54d0108a
cataezaru <cataezaru@gmail.com>
2022-05-30 22:42:12 +0300

    Fixed most of the phone errors Fixed fogotten name change on the header

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,7 @@
-import React, { useState } from 'react';
-import {RiMenu3Line, RiCloseLine} from 'react-icons/ri';
-import logo from '../../assets/logo.css'
-import './navbar.css';
-import '../../assets/Special.css'
+import React, { useState } from "react";
+import { RiMenu3Line, RiCloseLine } from "react-icons/ri";
+import "./navbar.css";
+import "../../assets/Special.css";
 
 const Navbar = () => {
   const [toggleMenu, setToggleMenu] = useState(false);
@@ -17,33 +16,63 @@ const Navbar = () => {
           <div className="loader" />
         </div>
         <div className="gpt3__navbar-links_container">
-          <p><a href="#home">Home</a></p>
-          <p><a href="#wgpt3">Who is Ouroboros</a></p>
-          <p><a href="#possibility">Possibilities</a></p>
-          <p><a href="#features">Technology</a></p>
-          <p><a href="#blog">Blog</a></p>
+          <p>
+            <a href="#home">Home</a>
+          </p>
+          <p>
+            <a href="#wgpt3">Who is Ouroboros</a>
+          </p>
+          <p>
+            <a href="#possibility">Possibilities</a>
+          </p>
+          <p>
+            <a href="#features">Technology</a>
+          </p>
+          <p>
+            <a href="#blog">Blog</a>
+          </p>
         </div>
-            </div>
-            <div className="gpt3__navbar-sign">
+      </div>
+      <div className="gpt3__navbar-sign">
         <button type="button">Contact Us</button>
       </div>
       <div className="gpt3__navbar-menu">
-        {toggleMenu
-          ? <RiCloseLine color="#fff" size={27} onClick={() => setToggleMenu(false)} />
-          : <RiMenu3Line color="#fff" size={27} onClick={() => setToggleMenu(true)} />}
+        {toggleMenu ? (
+          <RiCloseLine
+            color="#fff"
+            size={27}
+            onClick={() => setToggleMenu(false)}
+          />
+        ) : (
+          <RiMenu3Line
+            color="#fff"
+            size={27}
+            onClick={() => setToggleMenu(true)}
+          />
+        )}
         {toggleMenu && (
-        <div className="gpt3__navbar-menu_container scale-up-center">
-          <div className="gpt3__navbar-menu_container-links">
-            <p><a href="#home">Home</a></p>
-            <p><a href="#wgpt3">What is GPT3?</a></p>
-            <p><a href="#possibility">Open AI</a></p>
-            <p><a href="#features">Technology</a></p>
-            <p><a href="#blog">Blog</a></p>
+          <div className="gpt3__navbar-menu_container scale-up-center">
+            <div className="gpt3__navbar-menu_container-links">
+              <p>
+                <a href="#home">Home</a>
+              </p>
+              <p>
+                <a href="#wgpt3">Who is Ouroboros</a>
+              </p>
+              <p>
+                <a href="#possibility">Possibilities</a>
+              </p>
+              <p>
+                <a href="#features">Technology</a>
+              </p>
+              <p>
+                <a href="#blog">Blog</a>
+              </p>
+            </div>
+            <div className="gpt3__navbar-menu_container-links-sign">
+              <button type="button">Contact Us</button>
+            </div>
           </div>
-          <div className="gpt3__navbar-menu_container-links-sign">
-            <button type="button">Contact Us</button>
-          </div>
-        </div>
         )}
       </div>
     </div>
