89b8f073cdbe6bc7dac247ccdca0b5d70cd7da91
Yifalchubbee <Albert_c.david@yahoo.com>
2022-06-05 08:50:08 +0300

    .

--- src/index.js
+++ src/index.js
@@ -2,15 +2,12 @@ import React from 'react';
 import ReactDOM from "react-dom/client";
 import App from './App'
 import './index.css';
-import SupportAdmin from './components/supportadmin/SupportAdmin';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-const path = window.location.pathname
 
 root.render(
   <React.StrictMode>
-    { path.indexOf('/support') === -1 ? <App /> : <SupportAdmin /> }
-  </React.StrictMode>,
-  document.getElementById('root')
+    <App />
+  </React.StrictMode>
   
 );
